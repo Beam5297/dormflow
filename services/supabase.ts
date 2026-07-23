855d194cbd84e88b768734fd62c2beb385a33305
Beam5297 <aphisit2910259@gmail.com>
2026-07-23 20:42:16 +0700

    fix supabase anon key

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://gikucxbdaieiagzqsgzz.supabase.co';
-const supabaseKey = 'sb_publishable_XR8gTREO-Teb7z1oqdcqaQ_Jc9tFW-Q';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdpa3VjeGJkYWllaWFnenFzZ3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4MDU1MjUsImV4cCI6MjEwMDM4MTUyNX0.n7mY01UpSpEKymEhrmAT0ayQ18lvJLEaPeSNsoMOOBc';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
