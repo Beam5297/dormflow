0e49421d094720f46603e5983f01b3aa208e166c
Beam5297 <aphisit2910259@gmail.com>
2026-07-23 20:05:48 +0700

    update publishable key

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gikucxbdaeiagzqsqgzz.supabase.co';
+const supabaseUrl = 'https://gikucxbdaieiagzqsgzz.supabase.co';
 const supabaseKey = 'sb_publishable_XR8gTREO-Teb7z1oqdcqaQ_Jc9tFW-Q';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
